809b9d3a02f62710bb73415ab272606a9b277904
Sebastian Schepis <sschepis@gmail.com>
2023-01-09 14:05:22 -0700

    initial commit

--- dist/test/parser.test.js
+++ dist/test/parser.test.js
@@ -8,21 +8,5 @@ describe('Fug', () => {
             const fug = src_1.Fug.parse('div');
             (0, chai_1.expect)(fug).to.deep.equal({ name: 'div', attributes: {}, children: [], content: '' });
         });
-        it('should parse an element with attributes', () => {
-            const fug = src_1.Fug.parse('div(id=main, class=container)');
-            (0, chai_1.expect)(fug).to.deep.equal({ name: 'div', attributes: { id: 'main', class: 'container' }, children: [], content: '' });
-        });
-        it('should parse an element with children', () => {
-            const fug = src_1.Fug.parse('div[id=main, class=container](div, p)');
-            (0, chai_1.expect)(fug).to.deep.equal({ name: 'div', attributes: { id: 'main', class: 'container' }, children: ['div', 'p'], content: '' });
-        });
-        it('should parse an element with content', () => {
-            const fug = src_1.Fug.parse('div[id=main, class=container](`hello world`)');
-            (0, chai_1.expect)(fug).to.deep.equal({ name: 'div', attributes: { id: 'main', class: 'container' }, children: [], content: 'hello world' });
-        });
-        it('should parse an element with attributes, children and content', () => {
-            const fug = src_1.Fug.parse('div[id=main, class=container](div, p)(`hello world`)');
-            (0, chai_1.expect)(fug).to.deep.equal({ name: 'div', attributes: { id: 'main', class: 'container' }, children: ['div', 'p'], content: 'hello world' });
-        });
     });
 });
